9ee86bf28962d4c1b06d982df37620b293340e44
Katherina Curley <paytonagreen@gmail.com>
2020-09-10 17:58:10 -0500

    still fixing /public

--- frontend/components/Page.js
+++ frontend/components/Page.js
@@ -31,7 +31,7 @@ const Inner = styled.div`
 const GlobalStyle = createGlobalStyle`
 @font-face{
   font-family: 'radnika_next';
-  src: url('public/static/radnikanext-medium-webfont.woff2')
+  src: url('public/radnikanext-medium-webfont.woff2')
   format('woff2');
   font-weight: normal;
   font-style: normal;
