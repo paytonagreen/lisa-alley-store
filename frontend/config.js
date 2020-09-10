d1c4874f40fbfb070b0997f4125819a4ea4a91cf
Katherina Curley <paytonagreen@gmail.com>
2020-09-10 17:37:10 -0500

    updated to https

--- frontend/config.js
+++ frontend/config.js
@@ -1,5 +1,5 @@
 // This is client side config only - don't put anything in here that shouldn't be public!
 export const endpoint = `http://localhost:4444`;
-export const prodEndpoint = `http://lisa-alley-yoga-prod.herokuapp.com/`;
+export const prodEndpoint = `https://lisa-alley-yoga-prod.herokuapp.com/`;
 
 export const perPage = 6;
