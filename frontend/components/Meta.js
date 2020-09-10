e7ae21ecc6ece4a3d82efeddcf2927fb6057ddc0
Katherina Curley <paytonagreen@gmail.com>
2020-09-10 17:54:10 -0500

    updated to public dir

--- frontend/components/Meta.js
+++ frontend/components/Meta.js
@@ -4,8 +4,8 @@ const Meta = () => (
   <Head>
     <meta name="viewport" content="width=device-width, initial-scale=1"/>
     <meta charSet="utf-8"/>
-    <link rel="shortcut icon" href="/static/favicon.png"/>
-    <link rel="stylesheet" type ="text/css" href="/static/nprogress.css"/>
+    <link rel="shortcut icon" href="public/static/favicon.png"/>
+    <link rel="stylesheet" type ="text/css" href="public/static/nprogress.css"/>
     <title>Lisa Alley</title>
   </Head>
 );
