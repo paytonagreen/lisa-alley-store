a9041a1c9b24fdbb2a66087bc3d38d330d0274c4
Katherina Curley <paytonagreen@gmail.com>
2020-09-11 18:02:15 -0500

    SSL added

--- frontend/config.js
+++ frontend/config.js
@@ -1,5 +1,5 @@
 // This is client side config only - don't put anything in here that shouldn't be public!
 export const endpoint = `http://localhost:4444`;
-export const prodEndpoint = `http://api.paytongreen.com`;
+export const prodEndpoint = `https://api.paytongreen.com`;
 
 export const perPage = 6;
