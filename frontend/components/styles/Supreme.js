3fae5a8f5797f53706c9e8500f86784b063a8b31
Katherina Curley <paytonagreen@gmail.com>
2020-09-12 19:03:30 -0500

    added shipping/cart improvements

--- frontend/components/styles/Supreme.js
+++ frontend/components/styles/Supreme.js
@@ -1,13 +1,14 @@
 import styled from 'styled-components';
 
 const Supreme = styled.h3`
+  text-align: center;
   background: ${props => props.theme.red};
   color: white;
   display: inline-block;
   padding: 4px 5px;
   transform: skew(-3deg);
   margin: 0;
-  font-size: 4rem;
+  font-size: 3rem;
 `;
 
 export default Supreme;
