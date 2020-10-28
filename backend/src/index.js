31aaee3f0f753daf44f02b3921784b67ec75dc9d
Katherina Curley <paytonagreen@gmail.com>
2020-10-27 19:30:13 -0500

    quantity & featured updates

--- backend/src/index.js
+++ backend/src/index.js
@@ -35,7 +35,6 @@ let origin;
 server.express.use(async (req, res, next) => {
   const allowedOrigins = ['https://lisa-alley.com', 'https://store.lisa-alley.com', 'http://localhost:3000', 'http://localhost:7777'];
   origin = await req.headers.origin;
-  console.log(origin);
   if (allowedOrigins.includes(origin)) {
     res.setHeader('Access-Control-Allow-Origin', origin)
     res.setHeader('Access-Control-Allow-Headers', "X-PINGOTHER,Content-Type,X-Requested-With,accept,Origin,Access-Control-Request-Method,Access-Control-Request-Headers,Authorization")
