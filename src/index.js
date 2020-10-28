5d38462917d9cb1317c44ca3b890b7355355d6a3
Katherina Curley <paytonagreen@gmail.com>
2020-10-27 20:12:50 -0500

    url fix

--- src/index.js
+++ src/index.js
@@ -33,7 +33,7 @@ server.express.use(async (req, res, next) => {
 let origin;
 
 server.express.use(async (req, res, next) => {
-  const allowedOrigins = ['lisa-alley-store-git-dev.paytonagreen.vercel.app', 'https://lisa-alley.com', 'https://store.lisa-alley.com', 'http://localhost:3000', 'http://localhost:7777'];
+  const allowedOrigins = ['https://lisa-alley-store-git-dev.paytonagreen.vercel.app', 'https://lisa-alley.com', 'https://store.lisa-alley.com', 'http://localhost:3000', 'http://localhost:7777'];
   origin = await req.headers.origin;
   if (allowedOrigins.includes(origin)) {
     res.setHeader('Access-Control-Allow-Origin', origin)
